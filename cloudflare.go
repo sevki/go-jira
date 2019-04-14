5aff3c289f9dc4029ad83cfe3b2a8e04a4d03328
Sevki Hasirci <sevki@cloudflare.com>
2019-04-14 16:24:25 +0100

    use the cloudflared to get the tokens

--- cloudflare.go
+++ cloudflare.go
@@ -2,24 +2,47 @@ package jira
 
 import (
 	"net/http"
+	"net/url"
+
+	cftoken "github.com/cloudflare/cloudflared/cmd/cloudflared/token"
+
+	"golang.org/x/sync/syncmap"
 )
 
 const cookieheader = "cookie"
 
-func NewCloudflareAccessTransport(token string, t http.RoundTripper) http.RoundTripper {
-	if t == nil {
-		t = http.DefaultTransport
-	}
-	return &CloudflareAccessRoundTripper{token: token, t: t}
-}
-
 type CloudflareAccessRoundTripper struct {
-	token string
+	tokens *syncmap.Map
 
 	t http.RoundTripper
 }
 
+func NewCloudflareAccessTransport(t http.RoundTripper) http.RoundTripper {
+	if t == nil {
+		t = http.DefaultTransport
+	}
+	return &CloudflareAccessRoundTripper{t: t, tokens: new(syncmap.Map)}
+}
+
 func (c *CloudflareAccessRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
-	r.Header.Add("cf-access-token", c.token)
+	u := url.URL{
+		Host:   r.URL.Host,
+		Scheme: "https",
+	}
+	token, ok := c.tokens.Load(u.Host)
+	if !ok {
+		if tok, err := cftoken.GetTokenIfExists(&u); err == nil {
+			c.tokens.Store(u.Host, tok)
+			token = tok
+		}
+		if tok, err := cftoken.FetchToken(&u); err == nil {
+			c.tokens.Store(u.Host, tok)
+			token = tok
+		} else {
+			return nil, err
+		}
+	}
+
+	r.Header.Add("cf-access-token", token.(string))
 	return c.t.RoundTrip(r)
 }
